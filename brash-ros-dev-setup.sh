b7bc27367f035326195b6cea73683d46a3cf35e7
David Edell <david.edell@jhuapl.edu>
2023-10-16 12:50:08 -0400

    Docker-compose setup is now working with SBN, GSW, and CFDP traffic exchanging appropriately.

--- brash-ros-dev-setup.sh
+++ brash-ros-dev-setup.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 
-# Note: cFS setup piece may be redundant if using docker-compose
-echo "Check if using podman or docker and initialize cFS"
-podman --version
-if [[ $? -eq 0 ]]; then
-    MODE=podman
-#    ./setup-podman.sh
-else
-    MODE=docker
-    ./setup-docker.sh
-fi
-
-echo "MODE is $MODE"
+# NOTE: Use "docker-compose build" to build base images
 
 echo "Brash ROS Dev Setup"
-${MODE} run -it -v ${PWD}:/shared -w /shared/brash osrf/ros:galactic-desktop ./install.py
+${MODE} run --rm -it -v ${PWD}:/shared -w /shared/brash brash-ros checkout_and_install.sh
 if [[ $? -ne 0 ]]; then
-    echo "Brash ROS install.py execution failed. See above for details and retry."
+    echo "Brash ROS setup & build failed. See above for details and retry."
     exit 1;
 fi
 
