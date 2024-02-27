6ae1437b359fde7138c78570affb93db37f9b210
Ana Huaman <ana@traclabs.com>
2024-02-26 21:47:12 -0600

    Added scripts for building brash and cfe

---
+++ scripts/build_brash.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "##### Building brash #####"
+echo ""
+
+COMPOSE_FILE="docker-compose-dev.yml"
+CODE_DIR="/code"
+
+docker compose -f ${COMPOSE_FILE} run -w ${CODE_DIR}/brash rosgsw colcon build --symlink-install
+ret=$?
+if [ $ret -ne 0 ]; then
+  echo "!! Failed in colcon build step !!"
+  return 1  
+fi
+  
+echo ""
+echo "##### Done! #####"
+return 0
