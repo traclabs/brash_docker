e78c08d5c30513b7bff8277fd812b5110d0e44b8
Ana <ana@traclabs.com>
2024-02-26 23:41:14 -0600

    Update build_cfe.sh

--- scripts/build_cfe.sh
+++ scripts/build_cfe.sh
@@ -8,27 +8,32 @@ COMPOSE_FILE="docker-compose-dev.yml"
 CODE_DIR="/code"
 export UID=$UID
 
+echo "Run docker compose config.."
+docker compose -f ${COMPOSE_FILE} config
+
+echo "Print UID: ${UID}"
+echo "Try to run first compose"
 docker compose -f ${COMPOSE_FILE} run -w ${CODE_DIR}/cFS fsw make SIMULATION=native prep
 ret=$?
 if [ $ret -ne 0 ]; then
   echo "!! Failed in make SIMULATION step !!"
   return 1  
 fi
-  
+echo "Try to run second compose"  
 docker compose -f ${COMPOSE_FILE} run -w ${CODE_DIR}/cFS fsw make
 ret=$?
 if [ $ret -ne 0 ]; then
   echo "!! Failed in make step !!"
   return 1  
 fi
-
+echo "Print 3rd compose"
 docker compose -f ${COMPOSE_FILE} run -w ${CODE_DIR}/cFS fsw make install
 ret=$?
 if [ $ret -ne 0 ]; then
   echo "!! Failed in make install step !!"
   return 1  
 fi
-
+echo "ending..."
 echo ""
 echo "##### Done! #####"
 return 0
