cb2f9228ef728ca9ed6d0434ffd603cead3f8712
Ana C. Huaman Quispe <ana@traclabs.com>
2024-04-05 15:54:16 -0500

    Added juicer build step

--- scripts/build_brash.sh
+++ scripts/build_brash.sh
@@ -7,12 +7,22 @@ echo ""
 COMPOSE_FILE="docker-compose-dev.yml"
 CODE_DIR="/code"
 
+# Build BRASH workspace
 docker compose -f ${COMPOSE_FILE} run -w ${CODE_DIR}/brash rosgsw colcon build --symlink-install
 ret=$?
 if [ $ret -ne 0 ]; then
-  echo "!! Failed in colcon build step !!"
+  echo "!! Failed in colcon build step that builds brash workspace !!"
   return 1  
 fi
+
+# Build juicer
+docker compose -f ${COMPOSE_FILE} run -w ${CODE_DIR}/juicer rosgsw make
+ret=$?
+if [ $ret -ne 0 ]; then
+  echo "!! Failed in colcon build step that builds juicer !!"
+  return 1
+fi
+
   
 echo ""
 echo "##### Done! #####"
